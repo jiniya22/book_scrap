f16377f56600b4187adf2928933d680499876fd0
jini <jini@jiniworld.me>
2022-07-05 18:19:33 +0900

    :rewind:datasource 변경 mariadb -> PyMySQL

--- book_scrap/data/data_book.py
+++ book_scrap/data/data_book.py
@@ -1,28 +1,29 @@
-import mariadb
+import pymysql
 
-datasource = {'dbType': 'mariadb', 'host': '127.0.0.1', 'port': '3306', 'username': 'test', 'password': 'test',
+
+datasource = {'dbType': 'mysql', 'host': '127.0.0.1', 'port': '3306', 'username': 'test', 'password': 'test',
               'database': 'book'}
 
 
 def get_book_ids(isbns):
     ids = [None] * 10
 
-    with mariadb.connect(host=datasource['host'], user=datasource['username'], password=datasource['password'],
-                         db=datasource['database']) as conn:
-        with conn.cursor(named_tuple=True) as cur:
+    with pymysql.connect(host=datasource['host'], user=datasource['username'], password=datasource['password'],
+                         db=datasource['database'], charset="utf8", cursorclass=pymysql.cursors.DictCursor) as conn:
+        with conn.cursor() as cur:
             for i in range(len(isbns)):
                 sql = "SELECT id FROM book WHERE isbn13 = %s LIMIT 1" % isbns[i]
                 cur.execute(sql)
                 result = cur.fetchone()
                 if result:
-                    ids[i] = result.id
+                    ids[i] = result['id']
     return ids
 
 
 def update_best_seller(book_ids):
-    with mariadb.connect(host=datasource['host'], user=datasource['username'], password=datasource['password'],
-                         db=datasource['database']) as conn:
-        with conn.cursor(named_tuple=True) as cur:
+    with pymysql.connect(host=datasource['host'], user=datasource['username'], password=datasource['password'],
+                         db=datasource['database'], charset="utf8", cursorclass=pymysql.cursors.DictCursor) as conn:
+        with conn.cursor() as cur:
             for i in range(len(book_ids)):
                 if not book_ids[i]:
                     continue
