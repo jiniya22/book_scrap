a661e59bf40a9957ea117c195b0b390ef0a28344
jini <jini@jiniworld.me>
2022-07-12 15:59:25 +0900

    :truck: -

--- book_scrap/app_start.py
+++ book_scrap/app_start.py
@@ -1,15 +1,15 @@
-from best_seller import yes24
-from data import data_book
-from chaeking import book
+from best_seller import yes24 as best_seller_yes24
+from data import book as data_book
+from chaeking import book as chaeking_book
 
 
 def app():
-    isbns = yes24.get_isbns()
+    isbns = best_seller_yes24.get_isbns()
     book_ids = data_book.get_book_ids(isbns)
 
     for i in range(len(book_ids)):
         if not book_ids[i]:
-            book_ids[i] = book.get_id_by_isbn(isbns[i])
+            book_ids[i] = chaeking_book.get_id_by_isbn(isbns[i])
 
     data_book.update_best_seller(book_ids)
 
