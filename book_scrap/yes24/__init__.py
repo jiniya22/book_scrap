ecf79fa8d9f9850671fe5ed0489b4ca88a71f9ca
jini <jini@jiniworld.me>
2022-07-22 16:19:39 +0900

    :bug: ISBN13 가 웹 페이지상의 3번째 row가 아닐 경우에 대한 bug fix

--- book_scrap/yes24/__init__.py
+++ book_scrap/yes24/__init__.py
@@ -6,5 +6,9 @@ def get_isbn13(url):
     html = requests.get(url).text
     soup = BeautifulSoup(html, 'html.parser')
 
-    ele = soup.select_one('#infoset_specific table > tbody > tr:nth-child(3) > td')
+    tags = soup.select('#infoset_specific table > tbody > tr > th')
+    for i in range(len(tags)):
+        if tags[i].get_text() == 'ISBN13':
+            ele = soup.select_one('#infoset_specific table > tbody > tr:nth-child({}) > td'.format(i + 1))
+            break
     return ele.get_text() if ele else None
